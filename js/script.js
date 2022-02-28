ef9b03312353012d99d89caab863fdbad22d75f1
Aida Emilbekova <aida.emilbekova.kk@gmail.com>
2022-02-28 17:16:38 +0600

    Learn strings

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let myString= "Это строка";
+console.log(myString);
+
+let myString2= 'Это тоже строка';
+console.log(myString2);
+
+let myString3= 'He said:"I\'m a doctor"';
+console.log(myString3);
